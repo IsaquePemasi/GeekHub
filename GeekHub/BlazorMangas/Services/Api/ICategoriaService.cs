70ea6d3b38ba7dad46eac20a8e5f8008e95aab0f
IsaquePemasi <ADSisaque@gmail.com>
2024-01-02 16:57:19 -0300

    Otimizar o site #21

--- GeekHub/BlazorMangas/Services/Api/ICategoriaService.cs
+++ GeekHub/BlazorMangas/Services/Api/ICategoriaService.cs
@@ -5,9 +5,4 @@ public interface ICategoriaService
 {
     Task<List<CategoriaDTO>> GetCategorias();
     Task<CategoriaDTO> GetCategoria(int id);
-
-    //Task CreateCategoria(CategoriaDTO categoriaDto);
-    //Task UpdateCategoria(CategoriaDTO categoriaDto);
-    //Task DeleteCategoria(int id);
-    //Task<IEnumerable<CategoriaDTO>> LocalizaCategoria(string criterio);
 }
